78ec14bdd113e023bb12068cff4c2f9f421494a0
JoaoBernava <100872641+JoaoBernava@users.noreply.github.com>
2022-03-03 16:28:45 -0300

    Add files via upload

---
+++ ATIVIDADE - 03.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+    <title>Formulário de Contato</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+
+  <body>
+
+    <div class="container mx-auto bg-light mt-1 w-90">
+      <header class="row">
+        
+        <div id="barra_acessibilidade" class="d-none">
+          <ul id="atalhos">
+ <li><a href="menu" accesskey="1">Ir para menu [1]</a></li>
+ <li><a href="formulario" accesskey="2">Ir para formulário [2]</a></li>
+          </ul>   
+        </div>  
+
+
+       
+        <div class="col-12 col-sm-3">   
+<img src="logo.png" alt="Logotipo da empresa com uma montanha ao fundo.">
+        </div>
+
+        <div class="col-12 col-9">
+          <h1>Empresa Outstone</h1>
+        </div>
+      </header>
+
+      <section class="row">
+        <div class="d-none">
+         
+          <h2 id="menu">Menu</h2>
+        </div>
+        <div class="d-none">
+      
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Home</a>
+          <a class="dropdown-item" href="#">Contato</a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Desativado</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+  </div>
+</nav>
+       </nav>
+        </div>
+        <article class="col-12">  
+       <h2 id="formulario">Formulário de Contato</h2>
+
+   
+ <form method="POST">
+    <label for="exampleFormControlInput1">Nome*</label>
+    <input type="Nome" class="form-control" id="exampleFormControlInput1" placeholder="Nome*" required>
+    <label for="exampleFormControlInput1">Endereço de email*</label>
+    <input type="Email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com*" required>
+    <label for="exampleFormControlTextarea1">Mensagem*</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
+    <input type="submit" class="btn btn-primary my-1" value="Enviar">
+</form>
+
+        </article>
+      </section>
+
+      <footer class="row">
+        <div class="col-12">
+          <p>Copyright © 2022</p>
+        </div>
+      </footer>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <noscript>Atualize seu navegador</noscript>
+
+  </body>
+
+﻿﻿ ﻿﻿ ﻿</html>
